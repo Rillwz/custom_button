2552b8349bcf3b5ddf2704b48b56ae1bdee7579c
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-27 15:30:16 +0700

    custom button

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,58 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        RaisedButton(
-          color: Colors.amber,
-          child: Text('Raised Button'),
-          onPressed: () {},
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Custom Button'),
         ),
-      ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              RaisedButton(
+                color: Colors.amber,
+                child: Text('Raised Button'),
+                shape: StadiumBorder(),
+                onPressed: () {},
+              ),
+              Material(
+                borderRadius: BorderRadius.circular(20),
+                elevation: 3,
+                child: Container(
+                  width: 150,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [Colors.purple, Colors.pink],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.transparent,
+                    child: InkWell(
+                      splashColor: Colors.amber,
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: () {},
+                      child: Center(
+                          child: Text(
+                        'My Button',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
